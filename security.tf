ae1a6ac2b0e94824077feca7f52bd0bab142565a
UDAY PRATAP SINGH <31985102+udayfop@users.noreply.github.com>
2022-11-28 20:46:06 +0530

    Create security.tf

---
+++ security.tf
@@ -0,0 +1,24 @@
+module "vote_service_sg" {
+  source = "terraform-aws-modules/security-group/aws"
+
+  name        = "user-service"
+  description = "Security group for user-service with custom ports open within VPC, and PostgreSQL publicly open"
+  vpc_id      = "vpc-33fb3f58"
+
+  ingress_cidr_blocks      = ["172.31.0.0/16"]
+  ingress_rules            = ["https-443-tcp"]
+  ingress_with_cidr_blocks = [
+    {
+      from_port   = 8080
+      to_port     = 8090
+      protocol    = "tcp"
+      description = "User-service ports terraform"
+      cidr_blocks = "172.31.0.0/16"
+    },
+    {
+      rule        = "postgresql-tcp"
+      cidr_blocks = "0.0.0.0/0"
+    },
+  ]
+}
+
